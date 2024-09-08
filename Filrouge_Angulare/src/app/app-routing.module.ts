e61be9ce483f4b2ba61385b7833e27297dcffeba
hicham-oualla <hich2022am.oualla@gmail.com>
2024-09-08 22:05:22 +0100

    ajouter approutin aux projet

--- Filrouge_Angulare/src/app/app-routing.module.ts
+++ Filrouge_Angulare/src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { ClasseComponent } from './classe/classe.component';
 
 
 const routes: Routes = [
  {
     path: '',component: HomeComponent,
+  },
+  {
+    path: 'classes',component: ClasseComponent,
   }
   
 
