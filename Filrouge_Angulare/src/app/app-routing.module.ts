5a9d75971f946ca1251b50d993efbb8fb2a7e7bd
hicham-oualla <hich2022am.oualla@gmail.com>
2024-10-03 11:43:10 +0100

    update pour new component

--- Filrouge_Angulare/src/app/app-routing.module.ts
+++ Filrouge_Angulare/src/app/app-routing.module.ts
@@ -7,6 +7,9 @@ import { ClasseComponent } from './classe/classe.component';
 import { ClasseFormComponent } from './classe-form/classe-form.component';
 import { ApprenantsComponent } from './apprenants/apprenants/apprenants.component';
 import {ApprenantsFormComponent} from "./apprenants-form/apprenants-form.component";
+import {SuiviComponent} from "./suivi/suivi.component";
+import {LogineComponent} from "./logine/logine.component";
+import {RetardComponent} from "./retard/retard.component";
 
 
 const routes: Routes = [
@@ -27,7 +30,20 @@ const routes: Routes = [
   },
   {
     path: 'addapprenants/:id',component: ApprenantsFormComponent,
-  }
+  },
+  {
+    path: 'editApprenant/:id',component: ApprenantsFormComponent,
+  },
+  {
+    path: 'suivi/:id',component: SuiviComponent,
+  },
+
+  {
+    path: 'login',component:  LogineComponent,
+  },
+  {path:'retard/:id', component:RetardComponent}
+
+
 
 
 
