eea35527b49da78e1f7415f13a8f51205f3f25c4
hicham-oualla <hich2022am.oualla@gmail.com>
2024-10-03 11:44:18 +0100

    retard sevice

---
+++ Filrouge_Angulare/src/app/Services/retard.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import {Retard} from "../Model/reatrd";
+ // Adjust the path according to your structure
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RetardService {
+  private apiUrl = 'http://localhost:9095/api/retards'; // Replace with your actual API endpoint
+
+  constructor(private http: HttpClient) {}
+
+  // Save a new Retard
+  saveRetard(retard: Retard, apprenantId: number): Observable<Retard> {
+    return this.http.post<Retard>(`${this.apiUrl}/saveRetard&app/${apprenantId}`, retard);
+  }
+
+  // Get all Retards
+  getAllRetards(): Observable<Retard[]> {
+    return this.http.get<Retard[]>(`${this.apiUrl}/All`);
+  }
+
+  // Get Retard by ID
+  getRetardById(id: number): Observable<Retard> {
+    return this.http.get<Retard>(`${this.apiUrl}/getRetardById/${id}`);
+  }
+
+  // Update a Retard
+  updateRetard(id: number, retard: Retard): Observable<Retard> {
+    return this.http.put<Retard>(`${this.apiUrl}/updateRetard/${id}`, retard);
+  }
+
+  // Delete a Retard
+  deleteRetard(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/deleteRetard/${id}`);
+  }
+
+  // Count Retards by Apprenant
+  countRetardsByApprenant(apprenantId: number): Observable<number> {
+    return this.http.get<number>(`${this.apiUrl}/countRetardsByApprenant/${apprenantId}`);
+  }
+
+  // Get Retards by Apprenant ID
+  getRetardsByApprenant(apprenantId: number): Observable<Retard[]> {
+    return this.http.get<Retard[]>(`${this.apiUrl}/getRetardsByApprenant/${apprenantId}`);
+  }
+}
