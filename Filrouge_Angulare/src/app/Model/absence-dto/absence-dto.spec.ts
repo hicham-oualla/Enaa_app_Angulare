e2b9720a2bc966100c45d4b7a12c2c4ae87669ed
hicham-oualla <hich2022am.oualla@gmail.com>
2024-10-06 17:26:49 +0100

    creattion de class absence

---
+++ Filrouge_Angulare/src/app/Model/absence-dto/absence-dto.spec.ts
@@ -0,0 +1,7 @@
+import { AbsenceDTO } from './absence-dto';
+
+describe('AbsenceDTO', () => {
+  it('should create an instance', () => {
+    expect(new AbsenceDTO()).toBeTruthy();
+  });
+});
---
+++ Filrouge_Angulare/src/app/Model/absence-dto/absence-dto.ts
@@ -0,0 +1,8 @@
+export interface AbsenceDTO {
+  id?: number;
+  datedebut: Date;
+  datedefin: Date;
+  duration: number;
+  apprenantId: number;
+  etat_absence: string;
+}
