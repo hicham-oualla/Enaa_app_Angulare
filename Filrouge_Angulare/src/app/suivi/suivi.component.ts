f5ab05dbc13ae0e74981089d2170b18753682440
hicham-oualla <hich2022am.oualla@gmail.com>
2024-10-06 17:29:41 +0100

    ajouter methed toAbsence pour naviger vers la page de absence

--- Filrouge_Angulare/src/app/suivi/suivi.component.ts
+++ Filrouge_Angulare/src/app/suivi/suivi.component.ts
@@ -1,5 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from "@angular/router";
+import {ApprenantService} from "../Services/apprenant.service";
+import {RetardService} from "../Services/retard.service";
+import {Retard} from "../Model/reatrd";
 
 @Component({
   selector: 'app-suivi',
@@ -9,17 +12,35 @@ import {ActivatedRoute, Router} from "@angular/router";
 export class SuiviComponent implements OnInit{
 
   id!:number;
+  nom!:string;
+  nbrRetard!:number;
 
   constructor(private route: ActivatedRoute,
+              private service:ApprenantService,
+              private servicer : RetardService,
               private router:Router) {
   }
 
   ngOnInit(): void {
     this.id = this.route.snapshot.params['id'];
+    this.service.getApprenantById(this.id).subscribe(
+      data=>{
+        this.nom = data.nom;
+      }
+
+    )
+    this.servicer.countRetardsByApprenant(this.id).subscribe(
+      data=>{
+        this.nbrRetard = data
+      }
+    )
   }
 
   toRetartd(id:number){
     this.router.navigate(['retard', id])
+  }
+  toAbsence(id:number){
+    this.router.navigate(['absence', id])
   }
 
 }
